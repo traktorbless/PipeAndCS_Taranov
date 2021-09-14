a76e0c47a63eef2c68f7441ead39c0414a3e8f2e
Anton <aptaranov@yandex.ru>
2021-09-14 15:18:08 +0300

    add new parametrs for Pipe

--- PipeAndCS/PipeAndCS.cpp
+++ PipeAndCS/PipeAndCS.cpp
@@ -5,17 +5,38 @@ using namespace std;
 struct Pipe {
     int id;
     int diametr;
+    double length;
+    bool isInRepair  = false;
 };
 
 
 void printPipe(const Pipe& p) {
-    cout << "Pipe number of " << p.id << " with diametr " << p.diametr;
+    cout << "Pipe " << p.id << endl
+        <<"Her character: " << endl;
+    cout << "Diamter: " << p.diametr << endl;
+    cout << "Length: " << p.length << endl;
+    cout << "Repair satus: ";
+    if (!p.isInRepair) {
+        cout << "Station is work" << endl;
+    }
+    else {
+        cout << "Station does not work" << endl;
+    }
+}
+
+Pipe AddPipe() {
+    Pipe p = {};
+    cout << "Enter diametr: ";
+    cin >> p.diametr;
+    cout << "Enter length: ";
+    cin >> p.length;
+    return p;
 }
 
 int main()
 {
-    Pipe p = {};
-    cout << "Enter diametr: ";
-    cin >> p.diametr;
+    Pipe p = AddPipe();
     printPipe(p);
 }
+
+
