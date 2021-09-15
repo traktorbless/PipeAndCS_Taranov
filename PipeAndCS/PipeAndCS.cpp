fa23fc576ba71a667056eac6b001309004aa678d
traktorbless <aptaranov@yandex.ru>
2021-09-15 19:38:48 +0300

    added functions

--- PipeAndCS/PipeAndCS.cpp
+++ PipeAndCS/PipeAndCS.cpp
@@ -12,27 +12,27 @@ struct Pipe {
 struct CompressionStation {
     int id = 0;
     string name;
-    int countWorkshop;
-    int countWorkshopInAtive;
+    int numberWorkshop;
+    int numberWorkshopInAtive;
     double effiency;
 };
 
 Pipe AddPipe() {
-    Pipe p = {};
+    Pipe pipe = {};
     cout << "Enter diametr: ";
-    cin >> p.diametr;
+    cin >> pipe.diametr;
     cout << "Enter length: ";
-    cin >> p.length;
-    return p;
+    cin >> pipe.length;
+    return pipe;
 }
 
-void printPipe(const Pipe& p) {
-    cout << "Pipe " << p.id << endl
+void PrintPipe(const Pipe& pipe) {
+    cout << "Pipe " << pipe.id << endl
         <<"Her character: " << endl;
-    cout << "Diamter: " << p.diametr << endl;
-    cout << "Length: " << p.length << endl;
+    cout << "Diamter: " << pipe.diametr << endl;
+    cout << "Length: " << pipe.length << endl;
     cout << "Repair satus: ";
-    if (!p.isInRepair) {
+    if (!pipe.isInRepair) {
         cout << "Station is work" << endl;
     }
     else {
@@ -40,24 +40,66 @@ void printPipe(const Pipe& p) {
     }
 }
 
-CompressionStation addCS() {
-    CompressionStation c = {};
-    cout << "Enter name station: ";
-    cin >> c.name;
-    cout << "Enter number of workstaions: ";
-    cin >> c.countWorkshop;
-    cout << "Enter number of workstation in action: ";
-    int countStationinAction = 0;
-    cin >> countStationinAction;
-    // zakonchil tyt   
+void ChangePipe(Pipe& pipe){
+    pipe.isInRepair = !pipe.isInRepair;
 }
 
+CompressionStation AddCS() {
+    CompressionStation station = {};
+    cout << "Enter name station: ";
+    cin >> station.name;
+    cout << "Enter number of workstaions: ";
+    cin >> station.numberWorkshop;
+    cout << "Enter number of workstation in action: ";
+    int countStationAction = 0;
+    cin >> countStationAction;
+    while(countStationAction > station.numberWorkshop){
+        cout << "Invalid input" << endl;
+        cin >> countStationAction;
+    }
+    station.numberWorkshopInAtive = countStationAction;
+    cout << "Enter station effiency: ";
+    cin >> station.effiency;
+    return station;
+}
 
+void PrintCS(const CompressionStation& station) {
+    cout << "Compressor station " << station.id << endl
+    << "Her character: " << endl;
+    cout << "Name: " << station.name << endl;
+    cout << "Number of workstations: " << station.numberWorkshop << endl;
+    cout << "Number of workstations in action: " << station.numberWorkshopInAtive << endl;
+    cout << "Station effiency: " << station.effiency << endl;
+}
+
+void ChangeCs(CompressionStation& station,int newNumberWorkstationInAction) {
+    if(newNumberWorkstationInAction > station.numberWorkshop){
+        cout << "Invalid input" << endl;
+    } else {
+        station.numberWorkshopInAtive = newNumberWorkstationInAction;
+        cout << "New number of workshops: " << newNumberWorkstationInAction << endl;
+    }
+}
+
+void ShowAllObject(const CompressionStation& station, const Pipe& pipe){
+    PrintPipe(pipe);
+    PrintCS(station);
+}
+
+void PrintMenu(){
+    cout << "1.Add pipe" << endl;
+    cout << "2.Add compressor station" << endl;
+    cout << "3.Show all object" << endl;
+    cout << "4.Change pipe" << endl;
+    cout << "5.Change compressor station" << endl;
+    cout << "6.Save" << endl;
+    cout << "7.Load" << endl;
+    cout << "0.Exit" << endl;
+}
 
 int main()
 {
-    Pipe p = AddPipe();
-    printPipe(p);
+    PrintMenu();
 }
 
 
